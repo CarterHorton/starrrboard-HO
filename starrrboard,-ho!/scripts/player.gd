7e56d54e83cc76579854a9746b61223a7a9ec0f1
Chalvuk2 <calvin@hudak.xyz>
2025-11-08 14:57:06 -0600

    added animation

--- starrrboard,-ho!/scripts/player.gd
+++ starrrboard,-ho!/scripts/player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var rotation_speed :=200
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var gun = $gun
+@onready var thruster: AnimatedSprite2D = $Sprite2D/AnimatedSprite2D
 
 var laser_scene = preload("res://scenes/laser.tscn")
 func _physics_process(delta: float) -> void:
@@ -14,7 +15,10 @@ func _physics_process(delta: float) -> void:
 	var input_vector := Vector2(0,Input.get_axis("move_forward","move_backward"))
 	velocity += input_vector.rotated(rotation)*acceleration
 	velocity = velocity.limit_length(max_speed)
-	
+	if velocity:
+		thruster.play("thrusting")
+	else:
+		thruster.play("idle")
 	if Input.is_action_pressed("rotate_right"):
 		rotate(deg_to_rad(rotation_speed*delta))
 	if Input.is_action_pressed("rotate_left"):
