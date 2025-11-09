34a908925aa841254b411e68a24fdf88150cff35
Chalvuk2 <calvin@hudak.xyz>
2025-11-08 21:27:40 -0600

    finished cannon

--- starrrboard,-ho!/scripts/laser.gd
+++ starrrboard,-ho!/scripts/laser.gd
@@ -2,8 +2,15 @@ extends Area2D
 
 @export var speed := 500.0
 var movement_vector := Vector2(0,-1)
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	global_position += movement_vector.rotated(rotation)* speed * delta
+
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	await get_tree().create_timer(1.5).timeout     
 	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	animation_player.play("shoot")
