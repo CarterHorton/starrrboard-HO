f6a46c2e3a0070dffc32cd485ee7bb63b4ea0890
Chalvuk2 <calvin@hudak.xyz>
2025-11-09 08:35:56 -0600

    force pusni

--- starrrboard,-ho!/scripts/hub.gd
+++ starrrboard,-ho!/scripts/hub.gd
@@ -1,4 +1,4 @@
 extends Area2D
-#this is used to pull up the shop menu whenever you run into the hub
-func _on_body_entered(_body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/shop.tscn")
+##this is used to pull up the shop menu whenever you run into the hub
+#func _on_body_entered(_body: Node2D) -> void:
+	#get_tree().change_scene_to_file("res://scenes/shop.tscn")
