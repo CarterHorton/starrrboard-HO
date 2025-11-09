28e9c7d8b4a538ff8c4687bd53500b7a7cc36630
Chalvuk2 <calvin@hudak.xyz>
2025-11-08 23:08:01 -0600

    fixing shop wip

--- starrrboard,-ho!/scripts/player.gd
+++ starrrboard,-ho!/scripts/player.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 
-signal laser_shot(laser)
+#establish variables
+signal laser_shot(laser)													#signal that gets sent whenever the laser shoots
+@export var acceleration := 10												#acceleration constant
+@export var rotation_speed :=200											#max rotation speed constant
+@onready var camera_2d: Camera2D = $Camera2D								#access to camera node
+@onready var gun = $gun														#access to gun node
+@onready var thruster: AnimatedSprite2D = $Sprite2D/AnimatedSprite2D		#access to thruster node
+var laser_scene = preload("res://scenes/laser.tscn")						#preloads the laser to get fired
+var shoot_cd = false														#a boolean for if the cannon can get fired again
 
-@export var acceleration := 10
-@export var max_speed := 350
-@export var rotation_speed :=200
-@onready var camera_2d: Camera2D = $Camera2D
-@onready var gun = $gun
-@onready var thruster: AnimatedSprite2D = $Sprite2D/AnimatedSprite2D
+#establish changeable variables
+var cannonTimer = 1															#timer to switch the boolean
+@export var max_speed := 350												#max speed constant
 
-var laser_scene = preload("res://scenes/laser.tscn")
-var shoot_cd = false
-
-func _process(delta):
+#this function is how the actual cannonball if fired
+func _process(_delta):
 	if Input.is_action_pressed("shoot"):
 		if !shoot_cd:
 			shoot_cd=true
@@ -20,16 +23,21 @@ func _process(delta):
 			await get_tree().create_timer(1).timeout
 			shoot_cd=false
  
+#this is how the ship moves
 func _physics_process(delta: float) -> void:
-	
+	#get input direction
 	var input_vector := Vector2(0,Input.get_axis("move_forward","move_backward"))
+	#apply it
 	velocity += input_vector.rotated(rotation)*acceleration
 	velocity = velocity.limit_length(max_speed)
+	
+	#used to play animation
 	if velocity:
 		thruster.speed_scale = velocity.length()/400
 		thruster.play("thrusting")
 	else:
 		thruster.play("idle")
+	#rotation for the camera
 	if Input.is_action_pressed("rotate_right"):
 		rotate(deg_to_rad(rotation_speed*delta))
 	if Input.is_action_pressed("rotate_left"):
@@ -39,7 +47,7 @@ func _physics_process(delta: float) -> void:
 	
 
 	move_and_slide()
-
+#the call to shoot the laser
 func shoot_laser():
 	var l = laser_scene.instantiate()
 	l.global_position = gun.global_position
